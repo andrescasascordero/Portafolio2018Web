c08fe53d24efa4418fea7b2f5aaf7fa1f416f696
JCA <jcelemina@gmail.com>
2018-10-14 12:09:15 -0300

    hola andres porfaa revisaa el commomBC

---
+++ myOffersBLL/CommomBC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using myOffersDAL;
+
+namespace myOffersBLL
+{
+    public static class CommomBC
+    {
+        private static Entities _entities;
+
+        public static Entities entities
+        {
+            get
+            {
+
+                if (_entities == null)
+                {
+                    _entities = new Entities();
+                }
+                return _entities;
+            }
+
+        }
+
+
+    }
+}
